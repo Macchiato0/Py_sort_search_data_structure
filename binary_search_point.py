6bbf11fb97d87faec811b093a4871911e66432ef
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-09-29 16:50:53 -0400

    Update binary_search_point.py

--- binary_search_point.py
+++ binary_search_point.py
@@ -6,7 +6,7 @@ def binary_search(sorted_list, left_pointer, right_pointer, target):
 	
   # We calculate the middle index from the pointers now
   mid_idx = (left_pointer + right_pointer) // 2
-  mid_val = sorted_list[mid_idx]
+  mid_val = sorted_list[mid_idx][1][0]
 
   if mid_val == target:
     return mid_idx
@@ -16,8 +16,19 @@ def binary_search(sorted_list, left_pointer, right_pointer, target):
   if mid_val < target:
     # we reduce the sub-list by passing in a new left_pointer
     return binary_search(sorted_list, mid_idx + 1, right_pointer, target)
-  
+
+'''
 values = [77, 80, 102, 123, 288, 300, 540]
 start_of_values = 0
 end_of_values = len(values)
 result = binary_search(values, start_of_values, end_of_values, 288)
+'''
+
+for i in sp_shp:
+  target=i[1][0]	
+  result=binary_search(pt_shp,0,len(pt_shp),target)
+  if type(result) is int:
+    row=['sp',i[0]]	
+    pt_shp[result].extend(row)
+	
+  
