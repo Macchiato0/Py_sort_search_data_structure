7afb9aaca215fb04ba2779eab31e385b486de8d0
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-09-30 12:51:24 -0400

    Update route_find.py

--- route_find.py
+++ route_find.py
@@ -1,15 +1,18 @@
 #create a list of tlm as start point of a rout
+#pt list structure:
 pt_start=[i for i in pt_shp if 'tlm' in i]
 
 #data structure of route
 #rout [id,(line1,line2,line3),(point1,point2,point3)]
 
 '''
+#basic algorithm and data structure of function connect
+
 lines=[(1,2),(2,3),(3,4),(3,5),(2,12),(4,6),(6,7),(7,8),(8,100),(100,101),(10,11)]
 p=[1,10]
 
 lines1=[i for i in lines]
-'''
+
 
 result=[]
 def connect(lines,p):
@@ -25,7 +28,33 @@ def connect(lines,p):
     lines.remove(k)
   for j in match: 
     return connect(lines,j[1])
+'''
+result=[]
+def connect(lines,p):
+  global result
+  match=[]
+  for i in lines:
+    if i[1][0]==p:
+      match.append(i)
+  if len(match)>0:    
+    result.append(match)
+  #print match
+  for k in match:
+    lines.remove(k)
+  for j in match: 
+    return connect(lines,j[1])
+  
+#sec_network is a list of lists of lines. Each item in sec_network contains lines linked directly or indirectly to a transformer 
+#line1=[i for i in line_shp]
 
+sec_network=[] 
+ 
+for i in pt_start:
+  global result
+  result=[]
+
+connect(line1,680)
+sec_network.append(result)
  
 # connect(lines,1)
 # result is a 2nd network of a transformer
