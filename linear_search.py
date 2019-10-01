6a67dd22ebc4026fc5f7a7dcd37cb05cf06ba5dc
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-10-01 19:40:01 -0400

    Update linear_search.py

--- linear_search.py
+++ linear_search.py
@@ -1,7 +1,18 @@
 def linear_search(search_list, target_value):
   matches =[]
   for idx in range(len(search_list)):
-    if search_list[idx] == target_value:
+    if search_list[idx][1] == target_value:
       matches.append(idx)
   if matches:
     return matches
+
+#tlm_shp
+n=0
+for i in tlm_shp:
+  target=i[1]
+  result=linear_search(pt_shp,target)
+  if not result:
+    n+=1
+  else:
+    row=['tlm',i[0]]	
+    pt_shp[result].extend(row)
