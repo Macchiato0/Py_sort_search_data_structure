a2f88425660cfe18321c9a07702e47f584031354
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-10-07 12:03:10 -0400

    Update search_match.py

--- search_match.py
+++ search_match.py
@@ -16,20 +16,22 @@ SecOH=[i[0] for i in cursor]
 cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.SecUGElectricLineSegment',["SHAPE@"],where)
 SecUG=[i[0] for i in cursor]
 
-for i in sp:
-  pt=i[1].firstPoint
+def find_OH(sp_shp):
+  pt=sp_shp.firstPoint
   for j in SecOH:
     lp=j.lastPoint
     if lp.X==pt.X and lp.Y==pt.Y:
       SecOH.remove(j)
       i.append(j)
 
-      
-for i in [k for k in sp if len(k)<3]:
-  pt=i[1].firstPoint
+def find_UG(sp_shp):      
+  pt=sp_shp.firstPoint
   for j in SecUG:
     lp=j.lastPoint
     if lp.X==pt.X and lp.Y==pt.Y:
       SecUG.remove(j)
-      i.append(j)  
+      i.append(j)
+      
+for i in sp:
+  
 
