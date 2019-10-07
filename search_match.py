bbf17f84e37920d90787e11d47309b4d212e8734
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2019-10-07 11:45:13 -0400

    Update search_match.py

--- search_match.py
+++ search_match.py
@@ -1,6 +1,35 @@
-#collect data first
+#collect data of lines and pts
+fid='092101'
 
-cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.ServicePoint',["TLM"],where)
+where="feederid='{}'".format(fid)
 
-cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.Transformer',["SHAPE@"],where)
+cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.ServicePoint',["OID@","SHAPE@"],where+" and DEVICELOCATION is Null")
+sp=[[i[0],i[1]] for i  in cursor]
+
+cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.Transformer',["SHAPE@","TLM"],where)
+trans=[i for i in cursor]
+
+cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.SecOHElectricLineSegment'
+,["SHAPE@"],where)
+SecOH=[i[0] for i in cursor]
+
+cursor=arcpy.da.SearchCursor(r'E:\Data\yfan\Connection to dgsep011.sde\ELECDIST.ElectricDist\ELECDIST.SecUGElectricLineSegment',["SHAPE@"],where)
+SecUG=[i[0] for i in cursor]
+
+for i in sp:
+  pt=i[1].firstPoint
+  for j in SecOH:
+    lp=j.lastPoint
+    if lp.X==pt.X and lp.Y==pt.Y:
+      SecOH.remove(j)
+      i.append(j)
+
+      
+for i in [k for k in sp if len(k)<3]:
+  pt=i[1].firstPoint
+  for j in SecUG:
+    lp=j.lastPoint
+    if lp.X==pt.X and lp.Y==pt.Y:
+      SecUG.remove(j)
+      i.append(j)  
 
