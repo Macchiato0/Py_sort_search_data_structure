ff2ea5814af403466cd0be2afd7668d99e940688
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2020-02-04 16:22:39 -0500

    Create quick_sort_2020.py

---
+++ quick_sort_2020.py
@@ -0,0 +1,10 @@
+def partition(sorted_list,low,high)
+    i=low
+    pivot=sorted_list[high]
+    for j in range(low,high):
+        if sorted_list[j]<=pivot:
+            sorted_list[j],sorted_list[i]=sorted_list[i],sorted_list[j]
+            i =+ 1
+    sorted_list[high],sorted_list[low]=sorted_list[low],sorted_list[high]
+    
+    
