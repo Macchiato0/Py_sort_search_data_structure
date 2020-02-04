db5e88eb1f4ecc2ef558ab12dc8e9e90949bbdd5
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2020-02-04 16:27:14 -0500

    Update quick_sort_2020.py

--- quick_sort_2020.py
+++ quick_sort_2020.py
@@ -5,6 +5,14 @@ def partition(sorted_list,low,high)
         if sorted_list[j]<=pivot:
             sorted_list[j],sorted_list[i]=sorted_list[i],sorted_list[j]
             i += 1
-    sorted_list[high],sorted_list[low]=sorted_list[low],sorted_list[high]
-    
-    
+    sorted_list[high],sorted_list[i]=sorted_list[i],sorted_list[high]
+    return i
+
+def quick_sort(sorted_list,low,high):
+    if low<high:
+        pi=partition(sorted_list,low,high)
+        quick_sort(sorted_list,pi+1,high)
+        quick_sort(sorted_list,low,pi-1)
+
+low=0
+high=len(sorted_list)-1
