21ab10bff92fdfd46e275bd9d809b849956005be
Macchiato0 <50885101+Macchiato0@users.noreply.github.com>
2020-02-19 20:06:36 -0500

    Create Breadth_first_search.py

---
+++ Breadth_first_search.py
@@ -0,0 +1,13 @@
+class Vertex:
+	def __init__(self, n):
+		self.name = n
+		self.neighbors = list()
+		
+		self.distance = 9999
+		self.color = 'black'
+	
+	def add_neighbor(self, v):
+		if v not in self.neighbors:
+			self.neighbors.append(v)
+			self.neighbors.sort()
+
